08c1a4b6473feda672ebbd0e0a2d38186bdbf4b3
Oier Mees <oier.mees@gmail.com>
2024-07-01 16:46:37 -0700

    default workers

--- bridge_dataset/bridge_dataset_dataset_builder.py
+++ bridge_dataset/bridge_dataset_dataset_builder.py
@@ -190,8 +190,8 @@ class BridgeDataset(MultiThreadedDatasetBuilder):
     RELEASE_NOTES = {
         '1.0.0': 'Initial release.',
     }
-    N_WORKERS = 1  # number of parallel workers for data conversion
-    MAX_PATHS_IN_MEMORY = 1  # number of paths converted & stored in memory before writing to disk
+    N_WORKERS = 40  # number of parallel workers for data conversion
+    MAX_PATHS_IN_MEMORY = 80  # number of paths converted & stored in memory before writing to disk
     # -> the higher the faster / more parallel conversion, adjust based on avilable RAM
     # note that one path may yield multiple episodes and adjust accordingly
     PARSE_FCN = _generate_examples  # handle to parse function from file paths to RLDS episodes
