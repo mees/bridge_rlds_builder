adf270855b0b9514329e6b596eb57af8a8a53f89
Oier Mees <oier.mees@gmail.com>
2024-08-25 15:43:55 -0700

    1 worker

--- bridge_dataset/bridge_dataset_dataset_builder.py
+++ bridge_dataset/bridge_dataset_dataset_builder.py
@@ -219,7 +219,7 @@ class BridgeDataset(MultiThreadedDatasetBuilder):
     RELEASE_NOTES = {
         '1.0.0': 'Initial release.',
     }
-    N_WORKERS = 10  # number of parallel workers for data conversion
+    N_WORKERS = 1  # number of parallel workers for data conversion
     MAX_PATHS_IN_MEMORY = 20  # number of paths converted & stored in memory before writing to disk
     # -> the higher the faster / more parallel conversion, adjust based on avilable RAM
     # note that one path may yield multiple episodes and adjust accordingly
